dde98ce7a3d9a6d232123a66f442588f13fd3613
anoKTOK <anofilemath@yahoo.co.jp>
2021-02-07 18:11:47 +0900

    SETUP: extended

---
+++ 128extended/source/makeobj_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Deleting existing pak file..."
+for file in `find ../pakset -name '*.pak'`; do
+		rm $file
+done
+echo "done. Packing from dat file..."
+
+for file in `find . -name '*.dat'`; do
+    pakoption=`cat $file | grep "#!" | tr -d "#!" | tr -d " "`
+    if [ "$pakoption" = "" ]
+    then
+	echo "pak size is not specified. $file has been ignored."
+	continue
+    fi
+    pakname=`echo $file | sed -e "s/dat/pak/g"`
+    pakname=`basename $pakname`
+    pakname="../pakset/$pakname"
+    echo "searching $file"
+    echo "packing using $pakoption"
+    echo "packed in $pakname"
+    ./../bin/makeobj-extended $pakoption $pakname $file
+done
